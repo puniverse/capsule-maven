0eec49be4edf741ed9bf6b827e7c30d105bc9a68
pron <ron.pressler@gmail.com>
2016-12-19 21:10:53 +0200

    Copyright notice

--- src/main/java/MavenCapsule.java
+++ src/main/java/MavenCapsule.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, Parallel Universe Software Co. and Contributors. All rights reserved.
+ * Copyright (c) 2015-2016, Parallel Universe Software Co. and Contributors. All rights reserved.
  * 
  * This program and the accompanying materials are licensed under the terms 
  * of the Eclipse Public License v1.0, available at
